6eb5468cdde72bb0b51641690e08076d4bcc31f5
Jean Cornelio <jean_cornelio17@hotmail.com>
2024-08-13 01:53:53 -0400

    chart weather

--- src/components/ChartLine.jsx
+++ src/components/ChartLine.jsx
@@ -1,75 +1,171 @@
-import { useEffect, useRef } from 'react'
-import * as echarts from 'echarts'
+import React, { useEffect, useRef } from 'react';
+import * as am5 from '@amcharts/amcharts5';
+import * as am5xy from '@amcharts/amcharts5/xy';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 import { dateTransform } from '../helper'
-import { Condition } from './Condition'
+import { Condition } from './Condition';
 
-export const ChartLine = ({ currentDayData }) => {
-  const chart = useRef(null)
+export const ChartLine = ({currentDayData}) => {
+  const chartRef = useRef(null);
+  const count = useRef(null);
   const { date, hour, day } = currentDayData
 
+  const hourData = hour.map(el => {
+    return {
+      date: el.time,
+      value:  el.temp_c,
+      icon: el.condition.icon
+    }
+  })
+  
   useEffect(() => {
-    if (chart.current) {
-      let myChart = echarts.getInstanceByDom(chart.current)
+    if (chartRef.current) {
+      let root = am5.Root.new(chartRef.current);
 
-      if (myChart) {
-        myChart.dispose()
-      }
+      root.setThemes([
+        am5themes_Animated.new(root),
+      ]);
 
-      myChart = echarts.init(chart.current)
-      console.log(day)
-      const hours = hour.map((el) => dateTransform({ date: el.time, format: 'hh:mm' }))
-      const temp = hour.map((el) => el.temp_c)
-      console.log(temp)
-      myChart.setOption({
-        xAxis: {
-          type: 'category',
-          splitLine: {
-            show: true
-          },
-          data: hours
+      let chart = root.container.children.push(
+        am5xy.XYChart.new(root, {
+          panX: true,
+          panY: true,
+          wheelX: 'panX',
+          wheelY: 'zoomX',
+          layout: root.verticalLayout,
+          pinchZoomX: true,
+          paddingLeft: 0,
+        })
+      );
 
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            symbol: 'circle',
-            data: temp,
-            type: 'line',
-            smooth: true,
-            areaStyle: {},
-            lineStyle: {
-              color: '#5470C6'
-            },
+      let cursor = chart.set(
+        'cursor',
+        am5xy.XYCursor.new(root, {
+          behavior: 'none',
+        })
+      );
+      cursor.lineY.set('visible', false);
 
-            symbolSize: 5,
+      let colorSet = am5.ColorSet.new(root, {
+   
+      });
 
-            label: {
-              show: true
-            }
-          }
-        ]
-      })
-    }
 
-    return () => {
-      if (chart.current) {
-        const myChart = echarts.getInstanceByDom(chart.current)
-        if (myChart) {
-          myChart.dispose() // Clean up the chart instance
+
+      let xRenderer = am5xy.AxisRendererX.new(root, {
+        minorGridEnabled: true,
+        minGridDistance: 50,
+      });
+      xRenderer.grid.template.set('location', 0.5);
+      xRenderer.labels.template.setAll({
+        location: 0.5,
+        multiLocation: 0.5,
+      });
+
+      let xAxis = chart.xAxes.push(
+        am5xy.DateAxis.new(root, {
+          baseInterval: { timeUnit: 'hour', count: 1 },
+          renderer: xRenderer,
+          tooltip: am5.Tooltip.new(root, {}),
+        })
+      );
+
+      let yRenderer = am5xy.AxisRendererY.new(root, {});
+      yRenderer.grid.template.set('forceHidden', true);
+      yRenderer.labels.template.set('minPosition', 0.05);
+
+      let yAxis = chart.yAxes.push(
+        am5xy.ValueAxis.new(root, {
+          maxPrecision: 0,
+          extraMin: 0.1,
+          renderer: yRenderer,
+        })
+      );
+
+      let series = chart.series.push(
+        am5xy.LineSeries.new(root, {
+          xAxis: xAxis,
+          yAxis: yAxis,
+          valueYField: 'value',
+          valueXField: 'date',
+          maskBullets: false,
+          tooltip: am5.Tooltip.new(root, {
+            pointerOrientation: 'vertical',
+            dy: -20,
+            labelText: '{valueY}',
+          
+          }),
+        })
+      );
+
+      series.data.processor = am5.DataProcessor.new(root, {
+        dateFormat: 'yyyy-MM-dd HH:mm',
+        dateFields: ['date'],
+      });
+
+      series.strokes.template.setAll({
+        strokeDasharray: [3, 3],
+        strokeWidth: 2,
+      });
+
+      let i = -1;
+      let a = -1;
+      series.bullets.push(function () {
+        i++;
+
+        if (i > 7) {
+          i = 0;
         }
-      }
+
+        let container = am5.Container.new(root, {
+          centerX: am5.p50,
+          centerY: am5.p50,
+        });
+
+        container.children.push(
+          am5.Circle.new(root, { radius: 20, fill: series.get('fill') })
+        );
+
+        a++;
+     
+        container.children.push(
+         
+          am5.Picture.new(root, {
+            centerX: am5.p50,
+            centerY: am5.p50,
+            width: 35,
+            height: 35,
+            src: hourData[a].icon,
+          colorSet
+          })
+          
+        );
+
+        return am5.Bullet.new(root, {
+          sprite: container,
+        });
+      });
+
+      series.data.setAll(hourData);
+      series.appear(1000);
+
+      chart.appear(1000, 100);
+  
+      return () => {
+        root.dispose();
+      };
     }
-  }, [currentDayData])
+  }, [currentDayData]);
 
   return (
-    <div className='bg-white p-4 rounded-2xl flex flex-warp'>
-   {/*    <div className='bg-red-400 flex flex-col items-center w-[200px]'>
-      <h2 className='font-bold text-2xl text-sky-blue-500 inline-block py-1'>{date}</h2>
+    <div className='bg-white p-4 rounded-2xl flex flex-col items-center gap-1'>
+    {/*   <div className='bg-red-400'>
+   
       <Condition condition={day.condition} showTextCondition={true} />
       </div> */}
-      <div id="chart-container" className='h-[400px] w-full' ref={chart} ></div>
+      <div className='text-sky-blue-400 font-bold text-2xl mb-4'>{date}</div>
+      <div id="chartdiv" style={{ height: '250px', width: '100%' }} ref={chartRef}></div>;
     </div>
   )
-}
+};
+
